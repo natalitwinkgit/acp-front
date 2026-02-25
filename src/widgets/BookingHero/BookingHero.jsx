a6970b8851bdc39c2d5b9e833ccb5b284b27d3df
qazxcvbnjhytr <nataliasumska95@gmail.com>
2026-02-25 05:00:49 +0200

    style: update booking hero and form layout

--- src/widgets/BookingHero/BookingHero.jsx
+++ src/widgets/BookingHero/BookingHero.jsx
@@ -6,16 +6,17 @@ import BookingForm from "../BookingForm/BookingForm";
 export default function BookingHero() {
   return (
     <section className={styles.section}>
+      <div className={styles.mainBlock}>
+        <div className={styles.formBlock}>
+          <BookingForm />
 
-      <div className={styles.block}>
-        <BookingForm />
-
-        <div className={styles.imageWrap}>
-          <img
-            className={styles.image}
-            src="/BookingHero/main_photo_bus.png"
-            alt="Автобус"
-          />
+          <div className={styles.imageWrap}>
+            <img
+              className={styles.image}
+              src="/BookingHero/main_photo_bus.png"
+              alt="Автобус"
+            />
+          </div>
         </div>
       </div>
     </section>
