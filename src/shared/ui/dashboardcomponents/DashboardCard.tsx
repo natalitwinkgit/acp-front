81882f07a6eacda94df9ea31378cf2b9897f9425
afnesss <af.nesss07@gmail.com>
2026-05-07 11:47:12 +0200

    fix: renamed folder so the build is successful

--- src/shared/ui/dashboardcomponents/DashboardCard.tsx
+++ src/shared/ui/dashboardcomponents/DashboardCard.tsx
@@ -4,7 +4,7 @@ import styles from "./DashboardCard.module.css";
 type DashboardCardProps = HTMLAttributes<HTMLDivElement> & {
   children?: ReactNode;
 };
-
+//comment to trigger changes, ignor it
 export default function DashboardCard({
   children,
   className = "",
